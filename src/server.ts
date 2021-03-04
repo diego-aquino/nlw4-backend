22fc6108df038acf443fa707819ecc5b05d01665
Diego Aquino <diego.cruz001@gmail.com>
2021-03-04 15:19:14 -0300

    Implement route for creating users

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,14 @@
+import 'reflect-metadata';
 import express from 'express';
 import { SERVER_PORT } from './config';
 
+import './database';
+import routes from './routes';
+
 const app = express();
 app.use(express.json());
+
+app.use(routes);
 
 app.listen(SERVER_PORT, () => {
   console.log(`Server listening on port ${SERVER_PORT}...`);
