dd32657b75ffbb4f06a1f2649d026b970091cd7d
Diego Aquino <diego.cruz001@gmail.com>
2021-03-05 22:09:31 -0300

    Abstract users repository

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,13 +1,13 @@
 import { Request, Response } from 'express';
-import { getRepository } from 'typeorm';
+import { getCustomRepository } from 'typeorm';
 
-import User from '../models/User';
+import UsersRepository from '../repositories/UsersRepository';
 
 class UserController {
   async create(request: Request, response: Response): Promise<Response> {
     const { name, email } = request.body;
 
-    const usersRepository = getRepository(User);
+    const usersRepository = getCustomRepository(UsersRepository);
 
     const userAlreadyExists = await usersRepository.findOne({ email });
     if (userAlreadyExists) {
@@ -17,7 +17,7 @@ class UserController {
     const user = usersRepository.create({ name, email });
     await usersRepository.save(user);
 
-    return response.status(201).send(user);
+    return response.status(201).json(user);
   }
 }
 
