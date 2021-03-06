714cf4348bea830ab580b3cf26fcf5d7ac8d17fa
Diego Aquino <diego.cruz001@gmail.com>
2021-03-05 22:10:15 -0300

    Support creating surveys

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,13 @@
 import { Router } from 'express';
+import SurveysController from './controllers/SurveysController';
 import UserController from './controllers/UserController';
 
 const router = Router();
 
 const userController = new UserController();
 router.post('/users', userController.create);
+
+const surveyController = new SurveysController();
+router.post('/surveys', surveyController.create);
 
 export default router;
